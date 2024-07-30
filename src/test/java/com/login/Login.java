cd5b365bd15a78e24177a3703c14b73c8fa4b4aa
Rahul-Srinivasan56 <rahulsrinivasan56@gmail.com>
2024-07-30 10:27:58 +0530

    add new files

---
+++ src/test/java/com/login/Login.java
@@ -0,0 +1,29 @@
+package com.login;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.edge.EdgeDriver;
+
+public class Login {
+
+	public static void main(String[] args) {
+		
+        WebDriver driver = new EdgeDriver();
+        driver.get("https://www.facebook.com/login.php");
+        driver.findElement(By.id("email")).sendKeys("Rahul");
+        driver.findElement(By.id("pass")).sendKeys("Rahul@123");
+        driver.findElement(By.id("loginbutton")).click();
+        WebElement text = driver.findElement(By.xpath("//span[contains(text(),'Log in ')]"));
+        String text2 = text.getText();
+        System.out.println(text2);
+        //driver.quit();
+        WebElement errortxt = driver.findElement(By.xpath("//div[@class='_9ay7']"));
+        String er = errortxt.getText(); 
+        System.out.println(er);
+        driver.close();
+
+		
+	}
+
+}
